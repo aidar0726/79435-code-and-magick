6ddb351ca245ee62a2254aaa7b9660e08bdd1e36
aidar0726 <shakirov0726@gmail.com>
2017-08-14 21:18:56 +0600

    Исправил замечания

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,14 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  var max = -1;
+  var HISTOGRAMHEIGHT = 150;
+  var INITIALX = 150;
+  var WIDTHGISTOGRAMM = 40;
+  var YOURINITIAL = 'Вы';
+  var SPACEGISTOGRAMM = 90;
+  var TEXTNAMEY = 260;
+  var TEXTTIMEY = 80;
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
@@ -13,7 +21,6 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 150, 40);
   ctx.fillText('Список результатов:', 150, 60);
 
-  var max = -1;
 
   for (var y = 0; y < times.length; y++) {
     var time = times[y];
@@ -21,29 +28,25 @@ window.renderStatistics = function (ctx, names, times) {
       max = time;
     }
   }
-
-  var HISTOGRAMHEIGHT = 150;
-  var INITIALX = 150;
-  var WIDTHGISTOGRAMM = 40;
   var step = HISTOGRAMHEIGHT / max;
 
   for (var i = 0; i < times.length; i++) {
-    var colorSaturation = Math.floor(Math.random() * (207 - 50 + 1)) + 50;
+    var colorSaturation = Math.floor(Math.random() * 158) + 50;
     var currentName = names[i];
     var currentTime = times[i];
     var personalHeight = currentTime * step;
     var DIFFERENCEHEIGHT = HISTOGRAMHEIGHT - personalHeight;
-    var YOURINITIAL = 'Вы';
+
     ctx.fillStyle = 'black';
-    ctx.fillText(currentName, INITIALX + 90 * i, 260);
-    ctx.fillText(currentTime.toFixed(0), INITIALX + 90 * i, 80 + DIFFERENCEHEIGHT);
+    ctx.fillText(currentName, INITIALX + SPACEGISTOGRAMM * i, TEXTNAMEY);
+    ctx.fillText(currentTime.toFixed(0), INITIALX + SPACEGISTOGRAMM * i, TEXTTIMEY + DIFFERENCEHEIGHT);
 
     ctx.fillStyle = 'rgba(29, 23, ' + colorSaturation + ', 1)';
 
     if (currentName === YOURINITIAL) {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    ctx.fillRect(INITIALX + 90 * i, 90 + DIFFERENCEHEIGHT, WIDTHGISTOGRAMM, personalHeight);
+    ctx.fillRect(INITIALX + SPACEGISTOGRAMM * i, SPACEGISTOGRAMM + DIFFERENCEHEIGHT, WIDTHGISTOGRAMM, personalHeight);
   }
 
 };
